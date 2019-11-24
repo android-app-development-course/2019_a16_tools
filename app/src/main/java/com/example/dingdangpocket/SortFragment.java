d13fc884d990bf69ea267e74e1d9707de416cc36
GuoYongcong <871352780@qq.com>
2019-11-24 17:44:00 +0800

    添加了分类

--- app/src/main/java/com/example/dingdangpocket/SortFragment.java
+++ app/src/main/java/com/example/dingdangpocket/SortFragment.java
@@ -20,8 +20,8 @@ import java.util.Set;
 
 public class SortFragment extends Fragment {
     private final String[] mVals = new String[]
-            {"有信用卡", "有微粒贷", "我有房", "我有车", "有社保", "有公积金",
-                    "有人寿保险", "工资银行卡转账", "啥都没有"};
+            {"打字板", "花板", "表情制作", "带壳截图", "GIF合成分解", "取色器",
+                    "图片压缩", "图片拼接", "图片转链接"};
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,54 +30,55 @@ public class SortFragment extends Fragment {
         return view;
     }
     public void init(View view){
-
+        int id_flowlayout[] = {
+                R.id.id_flowlayout_1,
+                R.id.id_flowlayout_2,
+                R.id.id_flowlayout_3,
+                R.id.id_flowlayout_4,
+                R.id.id_flowlayout_5,
+                R.id.id_flowlayout_6
+        };
         //获取布局填充器,一会将tv.xml文件填充到标签内.
         final LayoutInflater mInflater = LayoutInflater.from(getActivity());
+        for(int id : id_flowlayout) {
+            //初始化布局和适配器,直接粘就行.
+            final TagFlowLayout mFlowLayout = view.findViewById(id);
+            mFlowLayout.setAdapter(new TagAdapter<String>(mVals) {
 
-
-        //初始化布局和适配器,直接粘就行.
-        final TagFlowLayout mFlowLayout = view.findViewById(R.id.id_flowlayout);
-        mFlowLayout.setAdapter(new TagAdapter<String>(mVals)
-        {
-
-            @Override
-            public View getView(com.zhy.view.flowlayout.FlowLayout parent, int position, String s)
-            {
+                @Override
+                public View getView(com.zhy.view.flowlayout.FlowLayout parent, int position, String s) {
 //                将tv.xml文件填充到标签内.
-                TextView tv = (TextView) mInflater.inflate(R.layout.flowlayout_item,
-                        mFlowLayout, false);
+                    TextView tv = (TextView) mInflater.inflate(R.layout.flowlayout_item,
+                            mFlowLayout, false);
 //               为标签设置对应的内容
-                tv.setText(s);
-                return tv;
-            }
-            //             为标签设置预点击内容(就是一开始就处于点击状态的标签)
-            @Override
-            public boolean setSelected(int position, String s)
-            {
-                return s.equals("Android");
-            }
-        });
+                    tv.setText(s);
+                    return tv;
+                }
+
+                //             为标签设置预点击内容(就是一开始就处于点击状态的标签)
+                @Override
+                public boolean setSelected(int position, String s) {
+                    return s.equals("Android");
+                }
+            });
 //          为点击标签设置点击事件.
-        mFlowLayout.setOnTagClickListener(new TagFlowLayout.OnTagClickListener()
-        {
-            @Override
-            public boolean onTagClick(View view, int position, com.zhy.view.flowlayout.FlowLayout parent)
-            {
-                Toast.makeText(getActivity(), mVals[position], Toast.LENGTH_SHORT).show();
-                //view.setVisibility(View.GONE);
-                return true;
-            }
-        });
+            mFlowLayout.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
+                @Override
+                public boolean onTagClick(View view, int position, com.zhy.view.flowlayout.FlowLayout parent) {
+                    Toast.makeText(getActivity(), mVals[position], Toast.LENGTH_SHORT).show();
+                    //view.setVisibility(View.GONE);
+                    return true;
+                }
+            });
 
 //          展示哪些标签处于选中状态,这个很重要我们设置标签可点击就是为了把用户选中状态的标签中的数据上传.
-        mFlowLayout.setOnSelectListener(new TagFlowLayout.OnSelectListener()
-        {
-            @Override
-            public void onSelected(Set<Integer> selectPosSet)
-            {
+            mFlowLayout.setOnSelectListener(new TagFlowLayout.OnSelectListener() {
+                @Override
+                public void onSelected(Set<Integer> selectPosSet) {
 
-            }
-        });
+                }
+            });
+        }
 
     }
 
