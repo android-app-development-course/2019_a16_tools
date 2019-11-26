9c4e970a3183e3aaf28347299d322f5b53ca1bf5
Chen-Xuming <1136447707@qq.com>
2019-11-26 15:07:33 +0800

    更改了启动页面

--- app/src/main/java/com/example/dingdangpocket/MainActivity.java
+++ app/src/main/java/com/example/dingdangpocket/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar);//恢复原有的样式
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
